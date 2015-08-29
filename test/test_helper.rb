87ab1ba2ccec0bbde820028af92a14564f82b064
kickinbahk <kickinbahk@gmail.com>
2015-08-28 20:33:47 -0700

    Add Extra Testing
    Add test to confirm email was downcase. Add test for further email checking "@foo..com" and regex to validate for it. Add exta minitest parameters to list all tests and pass/fail status rather than stopping on failed test.

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -2,10 +2,15 @@ ENV['RAILS_ENV'] ||= 'test'
 require File.expand_path('../../config/environment', __FILE__)
 require 'rails/test_help'
 require "minitest/reporters"
-Minitest::Reporters.use!
+Minitest::Reporters.use!(
+  Minitest::Reporters::SpecReporter.new,
+  ENV,
+  Minitest.backtrace_filter
+)
 
 class ActiveSupport::TestCase
   # Setup all fixtures in test/fixtures/*.yml for all tests in alphabetical order.
+  ActiveRecord::Migration.check_pending!
   fixtures :all
   include ApplicationHelper
   # Add more helper methods to be used by all tests here...
