738da458fdff02db97c8f97b3d6010db610b85e5
kickinbahk <kickinbahk@gmail.com>
2015-08-28 21:04:36 -0700

    testing the minitest reporter spec list feature

--- config/application.rb
+++ config/application.rb
@@ -23,9 +23,9 @@ module HartlSampleApp
     # Do not swallow errors in after_commit/after_rollback callbacks.
     config.active_record.raise_in_transactional_callbacks = true
 
-    config.generators do |g|
-      g.test_framework :minitest, spec: true
-    end
+#    config.generators do |g|
+#      g.test_framework :minitest, spec: true
+#    end
 
   end
 end
